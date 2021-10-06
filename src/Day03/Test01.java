70c4859e635c18f8064313ac09a3ac493f718c4b
zdy <52379181+zdy-ctrl@users.noreply.github.com>
2021-10-06 23:44:31 +0800

    年份对应的生肖

---
+++ src/Day03/Test01.java
@@ -0,0 +1,13 @@
+package Day03;
+/*
+案例需求：
+       编写一个程序，为一个给定的年份找出其对应的中国生肖。中国的生肖基于12年一个周期，每年用一个动物代表：
+       rat（鼠）、ox（牛）、tiger（虎）、rabbit（兔）、dragon（龙）、snake（蛇）、
+       horse（马）、sheep（羊）、monkey（候）、rooster（鸡）、dog（狗）、pig（猪）。
+       提示：2017年：鸡   2017 % 12 == 1
+*/
+public class Test01 {
+    public static void main(String[] args) {
+
+    }
+}
