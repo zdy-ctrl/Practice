dc3e20144b5e21eb8caec726b09a6b03a037ad51
zdy <52379181+zdy-ctrl@users.noreply.github.com>
2021-10-07 00:01:24 +0800

    年份对应的生肖

--- src/Day03/Test01.java
+++ src/Day03/Test01.java
@@ -1,4 +1,7 @@
 package Day03;
+
+import java.util.Scanner;
+
 /*
 案例需求：
        编写一个程序，为一个给定的年份找出其对应的中国生肖。中国的生肖基于12年一个周期，每年用一个动物代表：
@@ -8,6 +11,46 @@ package Day03;
 */
 public class Test01 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("请输入年份");
+        int year = in.nextInt();
+        switch (year % 12) {
+            case 1:
+                System.out.println("鸡年");
+                break;
+            case 2:
+                System.out.println("狗年");
+                break;
+            case 3:
+                System.out.println("猪年");
+                break;
+            case 4:
+                System.out.println("鼠年");
+                break;
+            case 5:
+                System.out.println("牛年");
+                break;
+            case 6:
+                System.out.println("虎年");
+                break;
+            case 7:
+                System.out.println("兔年");
+                break;
+            case 8:
+                System.out.println("龙年");
+                break;
+            case 9:
+                System.out.println("蛇年");
+                break;
+            case 10:
+                System.out.println("马年");
+                break;
+            case 11:
+                System.out.println("羊年");
+                break;
+            case 12:
+                System.out.println("猴年");
+                break;
+        }
     }
 }
