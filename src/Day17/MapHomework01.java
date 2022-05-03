0cb7a877ba65d72091ec1d6440702c9f7acb75ab
zdy <1812475641@qq.com>
2022-05-03 22:07:38 +0800

    Map作业1

---
+++ src/Day17/MapHomework01.java
@@ -0,0 +1,16 @@
+package Day17;
+
+import java.util.HashMap;
+
+/**
+ * @title:
+ * @author: ZDY
+ * @date: 2022/5/3 11:56
+ * @Abstract：
+ * 一个字符串，包含了空格等标点符号，写出一个函数计算出出现次数最多的字母和该字母出现的次数
+ */
+public class MapHomework01 {
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+    }
+}
